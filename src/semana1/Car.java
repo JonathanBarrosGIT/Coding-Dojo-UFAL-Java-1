372a54b36a97576f6906bbca3e32701a2f3af01b
Jonathan Barros <jxc3143@rit.edu>
2016-02-03 17:05:30 -0300

    Improvements done.

--- src/semana1/Car.java
+++ src/semana1/Car.java
@@ -5,19 +5,25 @@ package semana1;
  */
 public class Car extends Vehicle{
     private String type;
-    private String tPackage;
+    private boolean tPackage = false;
 
-    public Car(String model, String color, double cost, String type, String tPackage){
+    public Car(String model, String color, double cost, int type, int tPackage){
         super(model, color, cost);
-        this.type = type;
-        this.tPackage = tPackage;
+        if(type == 1)
+            this.type = "Sedan";
+        else if(type == 2)
+            this.type = "Coupe";
+        else
+            this.type = "Wagon";
+
+        this.tPackage = tPackage == 1;
     }
 
     public String getType(){
         return type;
     }
 
-    public String gettPackage(){
+    public boolean gettPackage(){
         return tPackage;
     }
 
--- src/semana1/Orders.java
+++ src/semana1/Orders.java
@@ -7,57 +7,95 @@ import java.util.*;
 import java.io.*;
 
 public class Orders {
-    public ArrayList orders = new ArrayList();
+    public ArrayList <Vehicle> orders;
+
+    Scanner scan = new Scanner(System.in);
+
+    public Orders() {
+        orders = new ArrayList();
+    }
+
+    public void menuCar(String model, String color, double cost){
+        System.out.print("What type of car is this?\n" +
+                "1. Sedan\n" +
+                "2. Coupe\n" +
+                "3. Wagon\n" +
+                "Choice: ");
+        int type = scan.nextInt();
+
+        System.out.print("Does this car have a towing package?\n" +
+                "1. Yes\n" +
+                "2. No\n" +
+                "Choice: "
+        );
+        int tPackage = scan.nextInt();
+
+        //Construct to Car
+        Car car = new Car(model, color, cost, type, tPackage);
+        orders.add(car);
+    }
+
+    private void menuTruck(String model, String color, double cost) {
+        System.out.print("What size Truck is this?\n" +
+                         "1. Half-ton\n" +
+                         "2. Full ton\n" +
+                         "Choice: ");
+        int capacity = scan.nextInt();
+
+        System.out.print("What is the engine size of the Truck?\n" +
+                         "1. Really big\n" +
+                         "2. Not so big\n +" +
+                         "Choice: ");
+        int engine = scan.nextInt();
+
+        //Construct to Truck
+        Truck truck = new Truck(model, color, cost, capacity, engine);
+        orders.add(truck);
+    }
 
     public void systemRun(){
+        Scanner scan = new Scanner(System.in);
         boolean loop = true;
         String choice;
-        Scanner scan = new Scanner(System.in);
+        String model;
+        String color;
+        double cost;
+
 
         while(loop){
             System.out.println("Do you want to order a Truck(T/t) or Car(C/c)?");
             choice = scan.nextLine();
 
             //Error message
-            if(choice.toLowerCase()!= "c" && choice.toLowerCase()!= "t"){
+            if(!choice.toLowerCase().equals("c") && !choice.toLowerCase().equals("t")) {
                 System.out.println("Please, choose between (T/t) for Trucks, and (C/c) for Cars.");
-            }
-
-            if(choice.toLowerCase() == "c"){
-
+            } else {
                 System.out.println("Entering Car order:");
                 System.out.print("Model: ");
-                String model = scan.nextLine();
+                model = scan.nextLine();
 
                 System.out.print("Color: ");
-                String color = scan.nextLine();
+                color = scan.nextLine();
 
                 System.out.print("Cost: ");
-                double cost = scan.nextDouble();
-
-                System.out.print("What type of car is this?\n" +
-                                    "1. Sedan\n" +
-                                    "2. Coupe\n" +
-                                    "3. Wagon\n" +
-                                    "Choice: ");
-
-
-                //Construct to Car
-                Car car = new Car(model, color, cost, type, tPackage);
-
-            } else {
-
+                cost = scan.nextDouble();
             }
 
-
+            if(choice.toLowerCase().equals("c")){
+                menuCar(model, color, cost);
+            } else {
+                menuTruck(model, color, cost);
+            }
+            System.out.print("Do you want to order another vehicle? (Y/y or N/n) ");
+            choice = scan.nextLine();
+            if(choice.toLowerCase().equals("n"))
+                loop = false;
         }
     }
 
     public static void main (String[] args){
 
+        Orders orders = new Orders();
+        orders.systemRun();
     }
-
-
-
-
 }
